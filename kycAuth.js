7bcc71be5b594f73cf48a50c5725e1ac8b9ecdb9
brqgoo <64094715+brqgoo@users.noreply.github.com>
2020-04-24 12:57:45 +0300

    Update kycAuth.js

--- kycAuth.js
+++ kycAuth.js
@@ -3,7 +3,7 @@
 
   var auth_button = document.getElementById(trust_id_auth);
   var auth_params = {
-    license_id:        'XXXXX-XXXXX-XXXXX-XXXXX', // place your business license here
+    license_key:        'XXXXX-XXXXX-XXXXX-XXXXX', // place your business license here
     scope:         {biometrics: ["given_name","nationality","gender","document_number"], version: 1},
     callback_url:  'https://example.com/callback/' // place callback url here
   };
@@ -15,9 +15,9 @@
   function kycAuth(options) {
 	
     var dynamic_link = 'https://trustid.page.link/?link=https://trust-id.co/resolve/'
-            + '&license_id=' + encodeURIComponent(options.bot_id)
-            + '&scope=' + encodeURIComponent(packScope(options.scope))
-
+            + stringToHex('?license_key=' + options.license_key)
+            + stringToHex('&scope=' + options.scope.biometrics)
+            + stringToHex('&callback_url=' + options.callback_url)
 
             + '&apn=com.gatepay.trustid&efr=1&ibi=com.gatepay.trustid&isi=MV47TP9PRQ'
     
@@ -26,61 +26,7 @@
     
   function openDeepLink(url) {
 	  
-    var is_ios       = /ios|iphone os|iphone|ipod|ipad/i.test(UA);
-    var is_firefox   = /firefox/i.test(UA);
-    var use_iframe   = (is_ios || is_firefox);
-    var use_once     = (!is_ios && is_firefox);
-    var timeOpen;
-    var ttNeedHide   = false;
-    var pageShown    = true;
-    var onPageHide   = function() {
-      pageShown = false;
-    };
-
-    if (use_iframe) {
-      var iframeEl = document.createElement('iframe');
-      iframeEl.style.position = 'absolute';
-      iframeEl.style.left = '-10000px';
-      iframeEl.style.top = '-10000px';
-      document.body.appendChild(iframeEl);
-      if (iframeEl !== null) {
-        timeOpen = +(new Date);
-        iframeEl.src = url;
-      }
-      if (!use_once) {
-        setTimeout(function() {
-          if (pageShown) {
-            timeOpen = +(new Date);
             window.location = url;
-          }
-        }, 2000);
-      }
-    }
-	
-    else {
-      setTimeout(function() {
-        timeOpen = +(new Date);
-        window.location = url;
-      }, 100);
-    }
-	
-  }
-
-function packScope(scope) {
-    if (scope.data) {
-      scope.d = scope.data;
-      delete scope.data;
-    }
-    if (!scope.d) {
-      throw new Error('scope data is required');
-    }
-    if (!scope.v) {
-      throw new Error('scope version is required');
-    }
-    for (var i = 0; i < scope.d.length; i++) {
-      scope.d[i] = packScopeField(scope.d[i]);
-    }
-    return JSON.stringify(scope);
   }
 
 
