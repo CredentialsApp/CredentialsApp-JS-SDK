f974de9bdcb361a465295bab3425911a828e6e70
brqgoo <64094715+brqgoo@users.noreply.github.com>
2020-06-06 18:41:41 +0300

    Update kycAuth.js

--- kycAuth.js
+++ kycAuth.js
@@ -1,7 +1,7 @@
-<button id="trust_id_auth">Verify via TrustID</button> 
+<button id="credentials_app_auth">Verify via Credentials App</button> 
   <script>
 
-  var auth_button = document.getElementById("trust_id_auth");
+  var auth_button = document.getElementById("credentials_app_auth");
 
   var auth_params = {
     license_key:   'XXXXX-XXXXX-XXXXX-XXXXX', // place your business license here
